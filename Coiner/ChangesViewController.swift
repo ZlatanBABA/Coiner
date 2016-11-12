0336593542c5e780bf48d518d5cc92999c86e8cb
LiuKangping <Leodian@leomac.local>
2016-11-12 20:00:48 +0100

    new page and gui elements

---
+++ Coiner/ChangesViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ChangesViewController.swift
+//  Coiner
+//
+//  Created by LiuKangping on 12/11/2016.
+//  Copyright © 2016 leomac. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+
+class ChangesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
